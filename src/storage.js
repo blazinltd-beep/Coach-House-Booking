8a2e17742175b90fe811f8676f8096eac0b01e73
blazinltd-beep <blazinltd@gmail.com>
2026-06-10 18:51:11 +0100

    Delete src directory

--- src/storage.js
+++
@@ -1,3 +0,0 @@
-const BK = 'chms_bookings'
-export const loadBookings = () => { try { const r = localStorage.getItem(BK); return r ? JSON.parse(r) : []; } catch { return []; } }
-export const saveBookings = (b) => { try { localStorage.setItem(BK, JSON.stringify(b)); } catch {} }
